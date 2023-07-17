60ef5877bcf9d8c5ddaf54192184b2e9d1f52d33
Ntillman7 <135862581+Ntillman7@users.noreply.github.com>
2023-07-17 17:26:59 -0600

    Create Polymorphism.py
    This is the file for the polymorphism submission assignment for the Tech Academy Bootcamp.

---
+++ polymorphism.py
@@ -0,0 +1,44 @@
+#parent class
+class Book:
+    title = 'unknown'
+    author = 'unknown'
+    number_of_pages = 0
+    book_format = 'Print'
+
+    def book_info(self):
+        msg = "\nTitle: {},\nAuthor: {},\nNumber of Pages: {},\nBook Format: {}".format(self.title,self.author,self.number_of_pages,self.book_format)
+        return msg
+
+    
+#child class
+class Horror(Book):
+    title = 'From Below'
+    author = 'Darcy Coates'
+    number_of_pages = 465
+    publisher = 'Poisoned Pen Press'
+    book_type = 'Paperback'
+
+    def book_info(self):
+        msg = "\nTitle: {},\nAuthor: {},\nNumber of Pages: {},\nBook Format: {}, \nPublisher: {},\nBook Type: {}".format(self.title,self.author,self.number_of_pages,self.book_format,self.publisher,self.book_type)
+        return msg
+    
+
+
+#child class
+class Classic(Book):
+    title = 'Journey to the Center of the Earth'
+    author = 'Jules Verne'
+    number_of_pages = 146
+    year_published = 1864
+    subgenre = 'Fiction'
+    
+    def book_info(self):
+        msg = "\nTitle: {},\nAuthor: {},\nNumber of Pages: {},\nBook Format: {},\nYear Published: {},\nSubgenre: {}".format(self.title,self.author,self.number_of_pages,self.book_format, self.year_published,self.subgenre)
+        return msg
+
+
+if __name__ == "__main__":
+    horror = Horror()
+    print(horror.book_info())
+    classic = Classic()
+    print(classic.book_info())
