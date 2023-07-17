c8997fdf0ab6f3f8293d69d36e43accd054b7b5f
Ntillman7 <135862581+Ntillman7@users.noreply.github.com>
2023-07-17 12:19:37 -0600

    Create Inheritance Assignment
    This is my assignment creating a parent class with 2 child classes to demonstrate inheritance.

---
+++ inheritance_assignment.py
@@ -0,0 +1,16 @@
+class Cat:
+    name = "no name"
+    breed = '  '
+    size = ' lb'
+#creating parent class Cat
+
+class Diet(Cat):
+    food_type = 'wet or dry'
+    preferred_flavor = 'flavor'
+#creating child class Diet
+
+class Grooming(Cat):
+    frequency = ' times per year'
+    previous_groom = 'date'
+#creating child class Grooming
+    
