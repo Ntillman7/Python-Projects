6027fe16180dc030aeedd3415945b0d5fc50b449
Ntillman7 <135862581+Ntillman7@users.noreply.github.com>
2023-07-21 18:07:27 -0600

    Update web_page_generator.py
    File updated with comments in the code.

--- web_page_generator.py
+++ web_page_generator.py
@@ -7,20 +7,25 @@ class ParentWindow(Frame):
     def __init__(self,master):
         Frame.__init__(self, master)
         self.master.title("Web Page Generator")
-
+        
+        #this creates the label and sets its orientation in the window
         self.lbl_customText = tk.Label(self.master,text='Enter custom text or click the Default HTML page button')
         self.lbl_customText.grid(row=0,column=0, padx=(30,30),pady=(10,10),sticky=N+W)
 
+        #this creates the entry field for the custom text
         self.txt_customText = tk.Entry(self.master, text='')
         self.txt_customText.grid(row=1,column=0, rowspan=1, columnspan=8, padx=(30,30),pady=(0,0),sticky=N+E+W)
 
+        #this created the default button
         self.btn = Button(self.master, text="Default HTML Page", width = 30, height=2, command=self.defaultHTML)
         self.btn.grid(row=3, column=3, padx=(10,10), pady=(10,10), sticky=E)
 
+        #this creates the button for the custom text entry
         self.btn = Button(self.master, text="Submit Custom Text", width = 30, height=2, command=self.submitText)
         self.btn.grid(row=3, column=4, padx=(10,30), pady=(10,10), sticky=E)
 
 
+    #this funtion allows for the new tab to opened with the default text
     def defaultHTML(self):
         htmlText = "Stay tuned for our amazing summer sale!"
         htmlFile = open("index.html", "w")
@@ -29,6 +34,8 @@ class ParentWindow(Frame):
         htmlFile.close()
         webbrowser.open_new_tab("index.html")
 
+
+    #this funtion allows for the new tab to be opened which will display the text entered in the box
     def submitText(self):
         htmlCustomText= self.txt_customText.get()
         htmlFile = open("index.html", "w")
