99e79148849af7c7a5ec3d89d8eab46c912aa265
Ntillman7 <135862581+Ntillman7@users.noreply.github.com>
2023-08-23 11:25:11 -0600

    Moved Django Projects
    Moved Django Projects into seperate file

--- DjangoUniversity/Classes/classApp/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from classApp.models import UniversityClasses
-from campusApp.models import UniversityCampus
-
-
-# Register your models here.
-admin.site.register(UniversityClasses)
-admin.site.register(UniversityCampus)
